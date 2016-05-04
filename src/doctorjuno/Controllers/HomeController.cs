21e6726d1d1f6e45c544b5e063694f0cb4ef26b2
Jackson Cafazzo <doctorjuno@gmail.com>
2016-05-04 14:59:34 -0700

    add home controller, jquery, materialize

---
+++ src/doctorjuno/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET /Home/About
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        //GET /Home/Contact
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        //GET: /Home/Error
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
